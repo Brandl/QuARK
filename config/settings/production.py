51cfcdf9b195d5bbec5ea0dde00205afcc17e7b1
Brandl <Brandl@users.noreply.github.com>
2020-03-04 23:58:59 +0100

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
+ALLOWED_HOSTS = ['*'] 
 ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", False)
 ACCOUNT_DEFAULT_HTTP_PROTOCOL="https"
 
